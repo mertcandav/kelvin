bf371c1a2accedf1a4acfeb481ca0b0336709b62
mertcandav <mertcandav@outlook.com>
2022-12-03 16:46:19 +0300

    add: buffering skills

--- kelvin.go
+++ kelvin.go
@@ -1,11 +1,8 @@
 package kelvin
 
 import (
-	"errors"
-	"fmt"
+	"encoding/json"
 	"os"
-	"path/filepath"
-	"reflect"
 )
 
 // Kelvin mode.
@@ -27,74 +24,26 @@ const emptyContent = "{}"
 type kelvin[T any] struct {
 	mode   byte
 	path   string
-	stack  []T
+	buffer []T
 	cipher Cipher
 }
 
-func readyToWrite(content []byte, cipher Cipher) []byte {
-	if cipher != nil {
-		content = cipher.Encrypt(content)
+// buff reads disk content of Kelvin database into buffer.
+func (k *kelvin[T]) buff() {
+	info, err := os.Stat(k.path)
+	if err != nil {
+		panic("buffering failed: path is not exist: " + k.path)
 	}
-	return content
-}
-
-// OpenSafe returns new instance of Kelvin by data type.
-// Panics if T is not structure.
-// Returns error if any errors occur.
-// Creates new Kelvin database if not exist in given path.
-// Uses given cipher for encryption.
-func OpenSafe[T any](path string, mode int, cipher Cipher) (*kelvin[T], error) {
-	var t T
-	kind := reflect.TypeOf(t).Kind()
-	if kind != reflect.Struct {
-		panic("type error: kelvin supports only structures")
+	if info.IsDir() {
+		panic("buffering failed: path is direcotry: " + k.path)
 	}
-
-	if mode != InMemory && mode != Strict {
-		return nil, fmt.Errorf("'%d' is invalid kelvin mode", mode)
+	bytes, err := os.ReadFile(k.path)
+	if err != nil {
+		panic("buffering failed: " + err.Error())
 	}
-
-	if path != NoWrite {
-		if filepath.Ext(path) != Ext {
-			return nil, fmt.Errorf(`"%s" path is not a kelvin database file`, path)
-		}
-
-		info, err := os.Stat(path)
-		if err != nil {
-			// Create new Kelvin database because not exist.
-
-			if !errors.Is(err, os.ErrNotExist) {
-				return nil, err
-			}
-			f, err := os.Create(path)
-			if err != nil {
-				return nil, err
-			}
-			content := []byte(emptyContent)
-			_, err = f.Write(readyToWrite(content, cipher))
-			if err != nil {
-				return nil, err
-			}
-			err = f.Close()
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			if info.IsDir() {
-				return nil, fmt.Errorf(`"%s" path is directory`, path)
-			}
-		}
+	bytes = readyToProcess(bytes, k.cipher)
+	err = json.Unmarshal(bytes, &k.buffer)
+	if err != nil {
+		panic("buffering failed: " + err.Error())
 	}
-
-	k := new(kelvin[T])
-	k.path = path
-	k.stack = nil
-	k.mode = byte(mode)
-	k.cipher = cipher
-	return k, nil
-}
-
-// Open same as OpenSafe, but not uses cipher.
-func Open[T any](path string, mode int) (*kelvin[T], error) {
-	return OpenSafe[T](path, mode, nil)
 }
---
+++ process.go
@@ -0,0 +1,93 @@
+package kelvin
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+)
+
+func readyToWrite(bytes []byte, cipher Cipher) []byte {
+	if cipher != nil {
+		bytes = cipher.Encrypt(bytes)
+	}
+	return bytes
+}
+
+func readyToProcess(bytes []byte, cipher Cipher) []byte {
+	if cipher != nil {
+		bytes = cipher.Decrpyt(bytes)
+	}
+	return bytes
+}
+
+// OpenSafe returns new instance of Kelvin by data type.
+// Returns error if any errors occur.
+// Creates new Kelvin database if not exist in given path.
+// Uses given cipher for encryption.
+//
+// Panics if;
+//  - T is not structure
+//  - decoding is failed
+//  - buffering is failed
+func OpenSafe[T any](path string, mode int, cipher Cipher) (k *kelvin[T], _ error) {
+	var t T
+	kind := reflect.TypeOf(t).Kind()
+	if kind != reflect.Struct {
+		panic("type error: kelvin supports only structures")
+	}
+
+	if mode != InMemory && mode != Strict {
+		return nil, fmt.Errorf("'%d' is invalid kelvin mode", mode)
+	}
+
+	var buffer []T
+	if path != NoWrite {
+		if filepath.Ext(path) != Ext {
+			return nil, fmt.Errorf(`"%s" path is not a kelvin database file`, path)
+		}
+
+		info, err := os.Stat(path)
+		if err != nil {
+			// Create new Kelvin database because not exist.
+
+			if !errors.Is(err, os.ErrNotExist) {
+				return nil, err
+			}
+			f, err := os.Create(path)
+			if err != nil {
+				return nil, err
+			}
+			content := []byte(emptyContent)
+			_, err = f.Write(readyToWrite(content, cipher))
+			if err != nil {
+				return nil, err
+			}
+			err = f.Close()
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			if info.IsDir() {
+				return nil, fmt.Errorf(`"%s" path is directory`, path)
+			}
+
+			if mode == InMemory {
+				defer k.buff()
+			}
+		}
+	}
+
+	k = new(kelvin[T])
+	k.path = path
+	k.buffer = buffer
+	k.mode = byte(mode)
+	k.cipher = cipher
+	return k, nil
+}
+
+// Open same as OpenSafe, but not uses cipher.
+func Open[T any](path string, mode int) (*kelvin[T], error) {
+	return OpenSafe[T](path, mode, nil)
+}
